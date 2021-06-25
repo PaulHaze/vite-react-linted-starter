70e14598c44508282688250e332850947396fb8b
PaulHaze <dev@artemist.com.au>
2021-06-25 12:04:44 +1000

    removed uneeded files

--- src/components/TestComp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function TestComp() {
-  return (
-    <div>
-      <h3>TestComp Placeholder''</h3>
-    </div>
-  );
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import store from './redux/store';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 
 import './css/main.css';
 import App from './App';
