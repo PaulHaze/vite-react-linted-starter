889236acbd106cba509feb146b855ccc8358dbbf
PaulHaze <dev@artemist.com.au>
2021-07-09 10:42:51 +1000

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,13 +46,13 @@ module.exports = {
     },
     container: {
       center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
+    },
+    padding: {
+      DEFAULT: '1rem',
+      sm: '2rem',
+      lg: '4rem',
+      xl: '5rem',
+      '2xl': '6rem',
     },
   },
 };
