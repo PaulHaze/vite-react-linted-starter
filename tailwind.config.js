ab8499ec9c7605fb3d6faccd8b3766ff06bd54a8
PaulHaze <dev@artemist.com.au>
2021-07-15 17:44:11 +1000

    add router and helmet packages, update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
     colors: {
       transparent: 'transparent',
