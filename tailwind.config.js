171fe612bce6d7e0908c452bad14890ea06d6ff9
PaulHaze <dev@artemist.com.au>
2021-08-28 17:22:31 +1000

    add more screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,11 @@ module.exports = {
       mlg: { max: '1024px' },
       mxl: { max: '1280px' },
       m2xl: { max: '1536px' },
+      smm: { min: '640px' },
+      mdm: { min: '768px' },
+      lgm: { min: '1024px' },
+      xlm: { min: '1280px' },
+      '2xlm': { min: '1536px' },
     },
     container: {
       center: true,
@@ -44,6 +49,7 @@ module.exports = {
         md: '768px',
         lg: '1024px',
         xl: '1280px',
+        '2xl': '1536px',
       },
       spacing: {
         sm: '8px',
