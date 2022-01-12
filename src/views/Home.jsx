3278ccba1a6417690640e0032b053aa7a7bcb90f
PaulHaze <dev@artemist.com.au>
2022-01-12 12:41:17 +1100

    styles clean-up

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export function Home() {
   return (
-    <div className="h-[100vh] bg-gradient-to-br from-teal-700/50 to-sky-500/50 flex items-center justify-center">
-      <div className="flex flex-col justify-center w-3/4 bg-white rounded-lg shadow-2xl">
-        <h2 className="m-10 text-[#504e63] font-bold text-center ">
+    <div className="h-[100vh] bg-gradient-to-br from-teal-700/50 to-sky-500/50 flex items-center justify-center p-md">
+      <div className="flex flex-col bg-white rounded-lg shadow-2xl">
+        <h2 className="m-10 text-[#504e63] font-bold text-center">
           Im your new Tailwind Project
         </h2>
         <p className="text-[#504e63] text-center mb-10">
